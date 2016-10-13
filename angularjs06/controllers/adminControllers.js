291614e7daa6401cc05a263cbdc67b66d94be843
myeongju.jung <redutan@gmail.com>
2016-10-14 00:00:39 +0900

    8장 완료

--- angularjs06/controllers/adminControllers.js
+++ angularjs06/controllers/adminControllers.js
@@ -3,7 +3,8 @@
  */
 angular.module('sportsStoreAdmin')
     .constant('authUrl', 'http://localhost:5500/users/login')
-    .controller('authCtrl', function ($scope, $http, $location, authUrl) {
+    .constant('ordersUrl', 'http://localhost:5500/orders')
+    .controller('authCtrl', function ($scope, $http, $location, authUrl, ordersUrl) {
         $scope.authenticate = function (user, pass) {
             console.log(user + ", " + pass);
             $http.post(authUrl, {
@@ -30,4 +31,27 @@ angular.module('sportsStoreAdmin')
             return $scope.current == "Products"
                 ? "/views/adminProducts.html" : "/views/adminOrders.html";
         };
+    })
+    .controller('ordersCtrl', function ($scope, $http, ordersUrl) {
+        $http.get(ordersUrl, {withCredentials : true})
+            .success(function (data) {
+                $scope.orders = data;
+            })
+            .error(function (data) {
+                $scope.error = error;
+            });
+
+        $scope.selectedOrder;
+
+        $scope.selectOrder = function (order) {
+            $scope.selectedOrder = order;
+        }
+
+        $scope.calcTotal = function (order) {
+            var total = 0;
+            for (var i = 0; i < order.products.length; i++) {
+                total += order.products[i].count * order.products[i].price;
+            }
+            return total;
+        }
     });
