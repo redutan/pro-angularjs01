445160964190fb40403a0fa655292169969e7005
myeongju.jung <redutan@gmail.com>
2016-10-13 14:34:37 +0900

    8장 예제 작업 중

--- angularjs06/controllers/sportsStore.js
+++ angularjs06/controllers/sportsStore.js
@@ -3,7 +3,8 @@
  */
 angular.module("sportsStore")
     .constant("dataUrl", "http://localhost:5500/products")
-    .controller("sportsStoreCtrl", function ($scope, $http, dataUrl) {
+    .constant("orderUrl", "http://localhost:5500/orders")
+    .controller("sportsStoreCtrl", function ($scope, $http, $location, dataUrl, orderUrl, cart) {
         $scope.data = {};
 
         $http.get(dataUrl)
@@ -13,4 +14,25 @@ angular.module("sportsStore")
             .error(function (error) {
                 $scope.data.error = error;
             });
+
+        $scope.sendOrder = function (shippingDetails) {
+            var order = angular.copy(shippingDetails);
+            order.products = cart.getProducts();
+            if (order.products.length == 0) {
+                alert("cart is empty!!");
+                $location.path("/product");
+                return;
+            }
+            $http.post(orderUrl, order)
+                .success(function (data) {
+                    $scope.data.orderId = data.id;
+                    cart.getProducts().length = 0;
+                })
+                .error(function (error) {
+                    $scope.data.orderError = error;
+                })
+                .finally(function () {
+                    $location.path("/complete");
+                });
+        };
     });
